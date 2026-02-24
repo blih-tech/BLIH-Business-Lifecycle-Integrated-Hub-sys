69e80e63746c94b0b0fea1de614820546a68042a
Amdebirhan Asmamaw <amdebrhanasmamaw93@gmail.com>
2026-02-24 17:15:51 +0300

    chore: rename @repo/types to @blih/types

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -5,5 +5,8 @@ module.exports = {
   'apps/web/**/*.{ts,tsx,js,jsx,mjs,cjs}': [
     'eslint --config apps/web/eslint.config.js --ignore-pattern apps/web/eslint.config.js --max-warnings=0 --fix --no-warn-ignored',
   ],
+  'packages/types/**/*.{ts,tsx,js,jsx,mjs,cjs}': [
+    'eslint --config packages/types/eslint.config.mjs --ignore-pattern packages/types/eslint.config.mjs --max-warnings=0 --fix --no-warn-ignored',
+  ],
   '*.{json,md,yml,yaml}': ['prettier --write'],
 };
